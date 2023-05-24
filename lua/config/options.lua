13bd68a8ccbb9e3337f25194c2d7f7a65ee053f5
dxy2233 <691369338@qq.com>
2023-05-24 12:32:56 +0800

    代码折叠设置

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 代码折叠
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99

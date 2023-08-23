72a382f8926a5694a95eaaa40432d8f8479d830e
dxy2233 <691369338@qq.com>
2023-08-23 10:17:22 +0800

    去除null-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,67 +12,90 @@ return {
           },
         },
         emmet_ls = {
-          filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug",
-            "typescriptreact", "vue" },
+          filetypes = {
+            "css",
+            "eruby",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "less",
+            "sass",
+            "scss",
+            "svelte",
+            "pug",
+            "typescriptreact",
+            "vue",
+          },
           init_options = {
             html = {
               options = {
                 ["bem.enabled"] = true,
               },
             },
-          }
+          },
         },
         volar = {
-          filetypes = { "vue" }
+          filetypes = { "vue" },
         },
       },
       setup = {
-        eslint = function()
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function(event)
-              if require("lspconfig.util").get_active_client_by_name(event.buf, "eslint") then
-                vim.cmd("EslintFixAll")
-              end
-            end,
-          })
-
-          require("lazyvim.util").on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
+        -- eslint = function()
+        --   vim.api.nvim_create_autocmd("BufWritePre", {
+        --     callback = function(event)
+        --       if require("lspconfig.util").get_active_client_by_name(event.buf, "eslint") then
+        --         vim.cmd("EslintFixAll")
+        --       end
+        --     end,
+        --   })
+        --
+        --   require("lazyvim.util").on_attach(function(client)
+        --     if client.name == "eslint" then
+        --       client.server_capabilities.documentFormattingProvider = true
+        --     elseif client.name == "tsserver" then
+        --       client.server_capabilities.documentFormattingProvider = false
+        --     end
+        --   end)
+        -- end,
         volar = function()
           require("lazyvim.util").on_attach(function(client)
-            if client.name == 'volar' then
+            if client.name == "volar" then
               client.server_capabilities.documentFormattingProvider = false
               client.server_capabilities.documentRangeFormattingProvider = false
             end
           end)
-        end
+        end,
       },
     },
   },
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "prettier")
+    end,
+  },
+  {
     "jose-elias-alvarez/null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    enabled = false,
+    -- event = { "BufReadPre", "BufNewFile" },
     dependencies = { "mason.nvim" },
-    opts = function()
-      -- local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          -- nls.builtins.formatting.fish_indent,
-          -- nls.builtins.diagnostics.fish,
-          -- nls.builtins.formatting.eslint,
-          -- nls.builtins.diagnostics.eslint,
-          -- nls.builtins.formatting.stylua,
-          -- nls.builtins.formatting.shfmt,
-          -- nls.builtins.diagnostics.flake8,
-        },
-      }
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.prettier.with({ filetypes = { "css", "scss" } }))
+      -- opts = function()
+      --   local nls = require("null-ls")
+      --   return {
+      --     root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      --     sources = {
+      --       -- nls.builtins.formatting.prettier,
+      --       -- nls.builtins.formatting.fish_indent,
+      --       -- nls.builtins.diagnostics.fish,
+      --       -- nls.builtins.formatting.eslint,
+      --       -- nls.builtins.diagnostics.eslint,
+      --       -- nls.builtins.formatting.stylua,
+      --       -- nls.builtins.formatting.shfmt,
+      --       -- nls.builtins.diagnostics.flake8,
+      --     },
+      --   }
     end,
   },
 }

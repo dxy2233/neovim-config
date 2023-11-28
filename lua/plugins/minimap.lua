49c2b5c4b17419ee460bd1c150440d5026d99e8f
dxy2233 <691369338@qq.com>
2023-11-28 15:09:26 +0800

    配置minimap

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,26 +1,38 @@
 return {
+  -- {
+  --   "petertriho/nvim-scrollbar",
+  --   event = "BufReadPost",
+  --   config = function()
+  --     local scrollbar = require("scrollbar")
+  --     local colors = require("dracula").colors()
+  --     scrollbar.setup({
+  --       handle = { color = "#ff8c94" },
+  --       excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "nerdtree" },
+  --       current_only = true,
+  --       base = "buffer",
+  --       column = 80,
+  --       signs_on_startup = { "all" },
+  --       -- marks = {
+  --       --   Search = { color = colors.orange },
+  --       --   Error = { color = colors.red },
+  --       --   Warn = { color = colors.cyan },
+  --       --   Info = { color = colors.green },
+  --       --   Hint = { color = colors.yellow },
+  --       --   Misc = { color = colors.purple },
+  --       -- },
+  --     })
+  --   end,
+  -- },
+
   {
-    "petertriho/nvim-scrollbar",
-    event = "BufReadPost",
-    config = function()
-      local scrollbar = require("scrollbar")
-      local colors = require("dracula").colors()
-      scrollbar.setup({
-        handle = { color = "#ff8c94" },
-        excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "nerdtree" },
-        current_only = true,
-        base = "buffer",
-        column = 80,
-        signs_on_startup = { "all" },
-        -- marks = {
-        --   Search = { color = colors.orange },
-        --   Error = { color = colors.red },
-        --   Warn = { color = colors.cyan },
-        --   Info = { color = colors.green },
-        --   Hint = { color = colors.yellow },
-        --   Misc = { color = colors.purple },
-        -- },
-      })
+    "wfxr/minimap.vim",
+    build = "cargo install --locked code-minimap",
+    lazy = false,
+    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+    init = function()
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
     end,
   },
 }

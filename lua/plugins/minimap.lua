5d3d72b73263b48391dcd1e89da4b7c4bb5d74ef
dxy2233 <691369338@qq.com>
2023-11-28 15:32:06 +0800

    scrollbar增加git

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,38 +1,45 @@
 return {
-  -- {
-  --   "petertriho/nvim-scrollbar",
-  --   event = "BufReadPost",
-  --   config = function()
-  --     local scrollbar = require("scrollbar")
-  --     local colors = require("dracula").colors()
-  --     scrollbar.setup({
-  --       handle = { color = "#ff8c94" },
-  --       excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "nerdtree" },
-  --       current_only = true,
-  --       base = "buffer",
-  --       column = 80,
-  --       signs_on_startup = { "all" },
-  --       -- marks = {
-  --       --   Search = { color = colors.orange },
-  --       --   Error = { color = colors.red },
-  --       --   Warn = { color = colors.cyan },
-  --       --   Info = { color = colors.green },
-  --       --   Hint = { color = colors.yellow },
-  --       --   Misc = { color = colors.purple },
-  --       -- },
-  --     })
-  --   end,
-  -- },
-
   {
-    "wfxr/minimap.vim",
-    build = "cargo install --locked code-minimap",
-    lazy = false,
-    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
-    init = function()
-      vim.cmd("let g:minimap_width = 10")
-      vim.cmd("let g:minimap_auto_start = 1")
-      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    "petertriho/nvim-scrollbar",
+    event = "BufReadPost",
+    config = function()
+      local scrollbar = require("scrollbar")
+      -- local colors = require("dracula").colors()
+      scrollbar.setup({
+        handle = { color = "#ff8c94" },
+        excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "nerdtree" },
+        current_only = true,
+        base = "buffer",
+        column = 80,
+        signs_on_startup = { "all" },
+        -- marks = {
+        --   Search = { color = colors.orange },
+        --   Error = { color = colors.red },
+        --   Warn = { color = colors.cyan },
+        --   Info = { color = colors.green },
+        --   Hint = { color = colors.yellow },
+        --   Misc = { color = colors.purple },
+        -- },
+      })
     end,
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      require("scrollbar.handlers.gitsigns").setup()
+    end,
+  },
+
+  -- {
+  --   "wfxr/minimap.vim",
+  --   build = "cargo install --locked code-minimap",
+  --   lazy = false,
+  --   cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+  --   init = function()
+  --     vim.cmd("let g:minimap_width = 10")
+  --     vim.cmd("let g:minimap_auto_start = 1")
+  --     vim.cmd("let g:minimap_auto_start_win_enter = 1")
+  --   end,
+  -- },
 }

615786887707c9c4d5723e00dd9077f060a7ee7a
dxy2233 <691369338@qq.com>
2023-11-28 11:17:32 +0800

    简单的滚动条

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,1 +1,26 @@
-return {}
+return {
+  {
+    "petertriho/nvim-scrollbar",
+    event = "BufReadPost",
+    config = function()
+      local scrollbar = require("scrollbar")
+      local colors = require("dracula").colors()
+      scrollbar.setup({
+        handle = { color = "#ff8c94" },
+        excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "nerdtree" },
+        current_only = true,
+        base = "buffer",
+        column = 80,
+        signs_on_startup = { "all" },
+        -- marks = {
+        --   Search = { color = colors.orange },
+        --   Error = { color = colors.red },
+        --   Warn = { color = colors.cyan },
+        --   Info = { color = colors.green },
+        --   Hint = { color = colors.yellow },
+        --   Misc = { color = colors.purple },
+        -- },
+      })
+    end,
+  },
+}

57850ebb708232a1fa2e1d5935d5426a4dd09817
dxy2233 <691369338@qq.com>
2023-12-12 13:02:22 +0800

    更新

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,12 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
--- 代码折叠
-opt.foldmethod = 'expr'
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
-opt.foldenable = false
-opt.foldlevel = 99
-
-opt.relativenumber = false
+-- local opt = vim.opt
+--
+-- -- 代码折叠
+-- opt.foldmethod = 'expr'
+-- opt.foldexpr = 'nvim_treesitter#foldexpr()'
+-- opt.foldenable = false
+-- opt.foldlevel = 99
+--
+-- opt.relativenumber = false
